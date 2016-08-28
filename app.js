1bf28867427df39d5f89fbe87a257320667a41a0
Sendiks <Justin.Mathieu@sendiksmarket.com>
2016-08-27 20:07:43 -0500

    0.0.2
    Adding additional assets and moving stuff around.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var domain = 'stream-fxpractice.oanda.com';
+var access_token = 'e27c1e3312c773948a0621a41abb91fe-17748f9960f7da53658a0dd9ee3f3434 ';
+var account_id = '4489830';
+var instruments = "EUR_USD%2CUSD_CAD";
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
